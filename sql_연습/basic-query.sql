86f5dbd571e29d7d16a9c006d0d0f055dc25abe3
shin-dongheon <tlsejdgjs@naver.com>
2023-08-30 09:08:55 +0900

    .

--- sql_연습/basic-query.sql
+++ sql_연습/basic-query.sql
@@ -43,3 +43,35 @@ delete from pet where name = '성타니';
 
 -- load data
 LOAD DATA LOCAL INFILE 'C:/Users/SDH/Documents/Poscodx/poscodx/mariadb-practices/pet.txt' into table pet;
+update pet set death = null;
+
+-- select data
+-- 문) bowser의 주인의 이름은?
+select owner from pet where name = 'bowser';
+
+-- 문2) 1998 이 후에 태어난 애들은?
+select * from pet where birth >= '1998-01-01';
+
+-- 문3) 종이 뱀이거나 새인 애들은?
+select * from pet where species = 'snake' or species = 'bird';
+
+-- 예4) order by
+select name, birth from pet order by birth asc;
+
+-- 예5) order by ~ desc
+select name, birth from pet order by birth desc;
+
+-- 예6) where 절에 null 다루기
+select name, birth death from pet where death is null;
+select name, birth death from pet where death is not null;
+
+-- 예7) like 검색 (패턴검색)
+select name from pet where name like 'b%';
+select name from pet where name like '%fy';
+select name from pet where name like '%w%';
+select name from pet where name like '____';
+select name from pet where name like 'b_____';
+
+-- 예8 ) 집계  count avg sum max min
+select count(*) from pet;
+select name, max(birth) from pet;
