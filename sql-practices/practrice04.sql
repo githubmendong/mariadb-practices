5fdfa44637eedc8d6478a12b179a1c563972cead
shin-dongheon <tlsejdgjs@naver.com>
2023-09-03 13:03:16 +0900

    .

--- sql-practices/practrice04.sql
+++ sql-practices/practrice04.sql
@@ -58,43 +58,85 @@ from employees e
 where s.salary > avg_salary_dept.avg_salary
 order by s.salary desc;
 
-
 -- 문제6.
--- 평균 연봉이 가장 높은 부서는?
-
-select d.dept_name as 부서, avg(s.salary)
-from departments d
-         join dept_emp de on d.dept_no = de.dept_no and de.to_date = '9999-01-01'
-         join salaries s on de.emp_no = s.emp_no and s.to_date = '9999-01-01'
-group by d.dept_no
-order by avg_salary desc limit 1;
-
+-- 평균 급여가 가장 높은 부서는?
+-- 부서이름, 평균급여
+SELECT
+    d.dept_name, ROUND(AVG(b.salary)) AS avg_salary
+FROM
+    employees a,
+    salaries b,
+    dept_emp c,
+    departments d
+WHERE
+        a.emp_no = b.emp_no
+  AND a.emp_no = c.emp_no
+  AND c.dept_no = d.dept_no
+  AND b.to_date = '9999-01-01'
+  AND c.to_date = '9999-01-01'
+GROUP BY d.dept_name  -- 부서 이름으로 그룹화해야 합니다.
+HAVING avg_salary = (SELECT
+                         MAX(avg_salary)
+                     FROM
+                         (SELECT
+                              ROUND(AVG(b.salary)) AS avg_salary
+                          FROM
+                              employees a, salaries b, dept_emp c
+                          WHERE
+                                  a.emp_no = b.emp_no
+                            AND a.emp_no = c.emp_no
+                            AND b.to_date = '9999-01-01'
+                            AND c.to_date = '9999-01-01'
+                          GROUP BY c.dept_no) AS subquery);
 
 -- 문제7.
--- 평균 연봉이 가장 높은 직책?
+-- 평균 급여가 가장 높은 직책?
+-- 직책, 평균급여
+SELECT
+    c.title, ROUND(AVG(b.salary)) AS avg_salary
+FROM
+    employees a,
+    salaries b,
+    titles c
+WHERE
+        a.emp_no = b.emp_no
+  AND a.emp_no = c.emp_no
+  AND b.to_date = '9999-01-01'
+  AND c.to_date = '9999-01-01'
+GROUP BY c.title
+HAVING avg_salary = (SELECT
+                         MAX(avg_salary)
+                     FROM
+                         (SELECT
+                              ROUND(AVG(b.salary)) AS avg_salary
+                          FROM
+                              employees a, salaries b, titles c
+                          WHERE
+                                  a.emp_no = b.emp_no
+                            AND a.emp_no = c.emp_no
+                            AND b.to_date = '9999-01-01'
+                            AND c.to_date = '9999-01-01'
+                          GROUP BY c.title) AS subquery);
 
-select t.title as 직책, avg(s.salary) as 평균연봉
-from titles t
-         join salaries s on t.emp_no = s.emp_no and s.to_date = '9999-01-01'
-group by t.title
-order by avg_salary desc limit 1;
 
 
 -- 문제8.
 -- 현재 자신의 매니저보다 높은 연봉을 받고 있는 직원은?
 -- 부서이름, 사원이름, 연봉, 매니저 이름, 메니저 연봉 순으로 출력합니다.
 
-
-select e.emp_no,
-       d.dept_name,
-       concat(e.first_name, ' ', e.last_name) as employee_name,
-       s.salary,
-       concat(m.first_name, ' ', m.last_name) as manager_name,
-       ms.salary                              as manager_salary
-from employees e
-         join dept_emp de on e.emp_no = de.emp_no and de.to_date = '9999-01-01'
-         join departments d on de.dept_no = d.dept_no
-         join employees m on de.emp_no = m.emp_no
-         join salaries s on e.emp_no = s.emp_no and s.to_date = '9999-01-01'
-         join salaries ms on m.emp_no = ms.emp_no and ms.to_date = '9999-01-01'
-where s.salary > ms.salary;
+SELECT
+    d.dept_name AS '부서이름',
+    CONCAT(e.first_name, ' ', e.last_name) AS '사원이름',
+    s.salary AS '연봉',
+    CONCAT(m.first_name, ' ', m.last_name) AS '매니저 이름',
+    ms.salary AS '매니저 연봉'
+FROM
+    employees e
+        JOIN dept_emp de ON e.emp_no = de.emp_no AND de.to_date = '9999-01-01'
+        JOIN departments d ON de.dept_no = d.dept_no
+        JOIN dept_manager dm ON de.dept_no = dm.dept_no AND dm.to_date = '9999-01-01'
+        JOIN employees m ON dm.emp_no = m.emp_no
+        JOIN salaries s ON e.emp_no = s.emp_no AND s.to_date = '9999-01-01'
+        JOIN salaries ms ON m.emp_no = ms.emp_no AND ms.to_date = '9999-01-01'
+WHERE
+        s.salary > ms.salary;
